40b0757b2dc1d6867454b8ae706b310363df48b9
RuslanN07 <natalinruslan4@gmail.com>
2025-11-12 20:11:36 +0400

    Update 2New.py

--- 2New.py
+++ 2New.py
@@ -5,7 +5,7 @@ def is_palindrome(s):
 
 num_words = {'0': 'ноль', '1': 'один', '2': 'два', '3': 'три'}
 
-# ЗАХВАТЫВАЕМ ВСЁ ЧИСЛО, если оно заканчивается на 1[13]
+
 pattern = re.compile(r'\b([0123]+1[13])\b')
 
 with open("2L.txt", encoding="utf-8") as f:
